84a00370ffe834ed6bba8b0cc2649f363edec26a
Руслан Хаиров <habur331@gmail.com>
2021-08-02 15:43:36 +0300

    Добавьте файлы проекта.

---
+++ Task1/Task1.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a, b, c;
+
+	cout << "a = ";
+	cin >> a;
+
+	cout << "b = ";
+	cin >> b;
+
+	cout << "c = ";
+	cin >> c;
+
+	long double D = b * 1.0 * b - 4.0 * a * c;
+
+	if (D < 0)
+	{
+		cout << "There is no answer";
+	}
+	else if (D == 0)
+	{
+		cout << -b * 1.0 / 2 * a;
+	}
+	else
+	{
+		cout << (-b * 1.0 - sqrt(D)) / 2 * a << endl << (-b * 1.0 + sqrt(D)) / 2 * a;
+	}
+}
+
